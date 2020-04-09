ea5297c041f22a59a723e25100e4f10694c78c4b
DavidLZisele <davidzisele2016@gmail.com>
2020-04-09 19:54:34 -0300

    arreglando cosas

--- sprint-5/resources/views/auth/login.blade.php
+++ sprint-5/resources/views/auth/login.blade.php
@@ -1,73 +1,88 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700|Montserrat:400,700&display=swap" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
+        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>LogIn</title>
+</head>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<body>
+    <div class="container col-12 col-md-12 col-lg-12 col-xl-12 contenedor-login">
+        <section>
+            <article>
+                <div class="col-2">
+                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user"
+                        class="svg-inline--fa fa-user fa-w-14 " role="img" xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 448 512">
+                        <path fill="currentColor"
+                            d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z">
+                        </path>
+                    </svg>
+                </div>
+                <form action="{{route('login')}}" method="POST">
+                    @csrf
+                    <p class="col-12">
+                        <label for="email" class="col-12">Usuario</label>
+                        <input type="text" name="email" id="email" class="col-9 form-control @error('email') is-invalid @enderror" value="{{old('email')}}">
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </p>
+                    <p class="col-12">
+                        <label for="password" class="col-12">Contraseña</label>
+                    <input type="password" name="password" id="password" class="col-9 form-control @error('password') is-invalid @enderror" value="{{old('password')}}">
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                     <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                   </p>
+                    <p class="col-12 recordar">
+                        <input type="checkbox" name="recordar" id="recordar" value="true"> 
+                        <span>
+                         Recordarme?
+                        </span>
+                    </p>
+                    <p class="col-12">
+                        <button type="submit" class="col-9">Ingresar</button>
+                    </p>
+                 
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                </form>
+            </article>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+        </section>
+        <footer id="footer-login" class="col-12 col-lg-12">
+            <div class="bloque-footer col-12">
+                 <div class="col-3">
+                    <a class="col-12" href="index.html">Inicio</a>
+                </div>
+                <div class="col-3">
+                    <a class="col-12" href="registro.php">Registro</a>
+                </div>
+                <div class="col-3">
+                    <a href="faq.php" class="col-12">F.A.Q</a>
+                </div>
+                <div class="col-3">
+                    <p>
+                        © 2019 Social.
+                    </p>
                 </div>
             </div>
-        </div>
+
+    </footer>
     </div>
-</div>
-@endsection
+</body>
+
+</html>
