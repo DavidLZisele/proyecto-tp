01e3f65a257a0a0b33f3068b7b98972ef5c45650
DavidLZisele <davidzisele2016@gmail.com>
2020-04-09 20:03:59 -0300

    arreglado registro

--- sprint-5/resources/views/auth/register.blade.php
+++ sprint-5/resources/views/auth/register.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,107 +15,112 @@
     <link rel="stylesheet" href="css/style.css">
     <title>Registro</title>
 </head>
-<div class="container col-12 col-md-12 col-lg-12 col-xl-12 contenedor-registro">
-    <div class="fondo col-lg-12 mb-5 mb-xl-0 col-xl-7">
 
-    </div>
-    <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
-        @csrf
+<body>
+    <div class="container col-12 col-md-12 col-lg-12 col-xl-12 contenedor-registro">
+        <div class="fondo col-lg-12 mb-5 mb-xl-0 col-xl-7">
 
-        <div class="form-group row">
-            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-              <div class="col-md-6">
-                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                    @error('name')
+        </div>
+        <section class="col-lg-12 col-xl-5 section-registro">
+        <form action="{{route('register')}}" method="POST" enctype ="multipart/form-data" class="form-registro">
+                @csrf
+                <div class="col-12 col-md-6 col-xl-12">
+                    <label class="col-6" for="email">Email</label>
+                    <input class="col-6 form-control @error('email') is-invalid @enderror" type="email" name="email" id="email" value="{{old('email')}}">
+                    <div class="bloque-error-registro  col-12">
+                        @error('email')
                         <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
                         </span>
-                     @enderror
-              </div>
-        </div>
-        <div class="form-group row">
-            <label for="surname" class="col-md-4 col-form-label text-md-right">{{ __('surname') }}</label>
-
-             <div class="col-md-6">
-                <input id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" required autocomplete="name" autofocus>
-
-                @error('surname')
-                  <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                   </span>
-                @enderror
-             </div>
-            </div>
-
-            <div class="form-group row">
-               <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                <div class="col-md-6">
-                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                    @error('email')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                         </span>
-                    @enderror
+                        @enderror
+                     </div> 
                 </div>
-             </div>
-
-            <div class="form-group row">
-               <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                <div class="col-md-6">
-                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
+                <div class="col-12 col-md-6 col-xl-12">
+                    <label class="col-6" for="password">Contraseña</label>
+                <input class="col-6 form-control @error('password') is-invalid @enderror" type="password" name="password" id="password" value="{{old('password')}}">
+                    <div class="bloque-error-registro  col-12">
                         @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                  <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                </div>
-             </div>
-
-             <div class="form-group row">
-                 <label for="photo" class="col-md-4 col-form-label text-md-right">{{ __('Photo') }}</label>
-
-                    <div class="col-md-6 photo">
-                       <input id="photo" type="file" class="form-control @error('photo') is-invalid @enderror" name="photo" value="{{ old('photo') }}" required autocomplete="name" autofocus>
-                        @error('photo')
-                           <span class="invalid-feedback" role="alert">
+                        <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
-                            </span>
+                        </span>
                         @enderror
-                            </div>
-                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-xl-12">
+                    <label class="col-6" for="name">Nombre</label>
+                <input class="col-6 form-control @error('name') is-invalid @enderror" type="text" name="name" id="name" value="{{old('name')}}">
+                    <div class="bloque-error-registro  col-12">
+                        @error('name')
+                        <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror 
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-xl-12">
+                    <label class="col-6" for="surname">Apellido</label>
+                <input class="col-6 form-control @error('surname') is-invalid @enderror" type="text" name="surname" id="surname" value="{{old('surname')}}">  
+                    <div class="bloque-error-registro  col-12">
+                        @error('surname')
+                        <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror 
+                    </div>
+                    
+                </div>
+                <div class="col-8 col-md-6 col-xl-8 bloque-foto-perfil">   
+                    <div class="bloque-foto col-6">
+                        <input class="col-12 foto_perfil form-control @error('photo') is-invalid @enderror" type="file" name="photo" id="foto-perfil-registro" > 
+                        <i class="fa fa-upload imagen-upload" aria-hidden="true"></i>
+                         <p >
+                            Foto
+                         </p>  
+                    </div>
+                      
+                    <div class="bloque-error-registro  col-12">
+                        @error('photo')
+                        <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                </div>
+                
+                <div class="col-12 col-md-6 col-xl-12 recordar-registro">
+                        <input type="checkbox" name="recordar" id="recordar" value="true"> 
+                        <span>
+                         Recordarme?
+                        </span>
+                </div>
+                <div class="col-12 col-md-6 col-xl-12 bloque-boton-registro">
+                    <button class="col-6" type="submit">Registrarte</button>
+                </div> 
+    </form>
+    </section>
 
-            <div class="form-group row mb-0">
-                <div class="col-md-6 offset-md-4">
-                       <button type="submit" class="boton-registro">
-                            {{ __('Register') }}
-                        </button>
-                 </div>
-              </div>
-      </form>
-      <footer id="footer-registro" class="col-12 col-lg-12 mt-auto">
-        <div class="bloque-footer col-12">
-             <div class="col-3">
-                <a class="col-12" href="{{route("home")}}">Inicio</a>
+    <footer id="footer-registro" class="col-12 col-lg-12 mt-auto">
+            <div class="bloque-footer col-12">
+                 <div class="col-3">
+                    <a class="col-12" href="index.html">Inicio</a>
+                </div>
+                <div class="col-3">
+                    <a class="col-12" href="login.php">LogIn</a>
+                </div>
+                <div class="col-3">
+                    <a href="faq.php" class="col-12">F.A.Q</a>
+                </div>
+                <div class="col-3">
+                    <p>
+                        © 2019 Social.
+                    </p>
+                </div>
             </div>
-            <div class="col-3">
-                <a class="col-12" href="{{route("login")}}">LogIn</a>
-            </div>
-            <div class="col-3">
-                <a href="faq.php" class="col-12">F.A.Q</a>
-            </div>
-            <div class="col-3">
-                <p>
-                    © 2019 Social.
-                </p>
-            </div>
-        </div>
 
-</footer>     
-</div>
+    </footer>
+
+    </div>
+
+</body>
+
 </html>
