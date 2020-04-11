d9076a8181bd2821e054f4ff9e551fbd76421837
Hernan Cefarelli <hernancefarelli@gmail.com>
2020-04-11 00:08:39 -0300

    Arreglado de css en editar foto posteo

--- sprint-5/resources/views/datosPub.blade.php
+++ sprint-5/resources/views/datosPub.blade.php
@@ -11,7 +11,7 @@
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700|Montserrat:400,700&display=swap" rel="stylesheet">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
         integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="/css/style.css">
     <title>Modificar Publicacion</title>
 </head>
 <body>
